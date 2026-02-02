6e550202c49934ddf70da58c293d06a57c8b6a7e
Arjun <150166325+Arjun0170@users.noreply.github.com>
2026-02-02 13:14:34 +0530

    Delete 16x16 directory

--- 16x16/xcelium.d/run.lnx8664.22.09.d/files.ts
+++
@@ -1,3 +0,0 @@
-1769585415 /home/ivsemvlsi/Downloads/Systolic-Tensors-main/reference/16x16/Systolic_Array_TB.sv
-1768976180 /home/ivsemvlsi/Downloads/Systolic-Tensors-main/reference/16x16/systolic_array.sv
-1768403251 /home/ivsemvlsi/Downloads/Systolic-Tensors-main/reference/16x16/mac_unit.sv
